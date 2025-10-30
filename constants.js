388423012139f4a9d8911e7cd63fc7ef60b89eaa
IsaacG185 <gisaac@wustl.edu>
2025-10-30 15:32:33 -0500

    Update constants.js

--- constants.js
+++ constants.js
@@ -14,10 +14,10 @@ window.constants = {
     displayName: "Planck constant (h)",
     definition: "Relates the energy of a photon to its frequency.",
     values: {
-      "J*s": 6.62607015*10^-34,
-      "erg*s": 6.62607015*10^-27,
-      "eV*s": 4.135667696*10^-15,
-      "keV*s": 4.135667696*10^-18
+      "J*s": 6.62607015e-34,
+      "erg*s": 6.62607015e-27,
+      "eV*s": 4.135667696e-15,
+      "keV*s": 4.135667696e-18
     }
   },
 
@@ -26,10 +26,10 @@ window.constants = {
     displayName: "Speed of Light (c)",
     definition: "Universal constant for the speed of light in vacuum.",
     values: {
-      "m*s^-1": 2.99792458*10^8,
-      "cm*s^-1": 2.99792458*10^10,
-      "km*s^-1": 2.99792458*10^5,
-      "Å*s^-1": 2.99792458*10^18
+      "m*s^-1": 2.99792458e8,
+      "cm*s^-1": 2.99792458e10,
+      "km*s^-1": 2.99792458e5,
+      "Å*s^-1": 2.99792458e18
     }
   },
 
@@ -38,9 +38,9 @@ window.constants = {
     displayName: "Boltzmann constant (k<sub>B</sub>)",
     definition: "Relates temperature to thermal energy.",
     values: {
-      "J*K^-1": 1.380649*10^-23,
-      "erg*K^-1": 1.380649*10^-16,
-      "eV*K^-1": 8.617333262*10^-5
+      "J*K^-1": 1.380649e-23,
+      "erg*K^-1": 1.380649e-16,
+      "eV*K^-1": 8.617333262e-5
     }
   },
 
@@ -49,7 +49,7 @@ window.constants = {
     displayName: "Stefan–Boltzmann constant (σ)",
     definition: "Constant of proportionality in Stefan–Boltzmann law.",
     values: {
-      "W*m^-2*K^-4": 5.670374419*10^-8
+      "W*m^-2*K^-4": 5.670374419e-8
     }
   },
 
@@ -58,7 +58,7 @@ window.constants = {
     displayName: "Universal Gravitational Constant (G)",
     definition: "Proportionality factor in Newton’s law of gravitation.",
     values: {
-      "m^3*kg^-1*s^-2": 6.67430*10^-11
+      "m^3*kg^-1*s^-2": 6.67430e-11
     }
   },
 
@@ -67,9 +67,9 @@ window.constants = {
     displayName: "Electronvolt (eV)",
     definition: "Energy gained by an electron when accelerated through 1 volt.",
     values: {
-      "J": 1.602176634*10^-19,
-      "erg": 1.602176634*10^-12,
-      "keV": 1*10^-3
+      "J": 1.602176634e-19,
+      "erg": 1.602176634e-12,
+      "keV": 1e-3
     }
   },
 
@@ -78,7 +78,7 @@ window.constants = {
     displayName: "Earth radius (R<sub>⊕</sub>)",
     definition: "Mean radius of Earth.",
     values: {
-      "m": 6.371^10^6,
+      "m": 6.371e6,
       "km": 6371
     }
   },
@@ -88,8 +88,8 @@ window.constants = {
     displayName: "Earth mass (M<sub>⊕</sub>)",
     definition: "Mass of Earth.",
     values: {
-      "kg": 5.972*10^24,
-      "g": 5.972*10^27
+      "kg": 5.972e24,
+      "g": 5.972e27
     }
   },
 
@@ -98,8 +98,8 @@ window.constants = {
     displayName: "Solar radius (R<sub>⊙</sub>)",
     definition: "Mean radius of the Sun.",
     values: {
-      "m": 6.957*10^8,
-      "km": 6.957*10^5
+      "m": 6.957e8,
+      "km": 6.957e5
     }
   },
 
@@ -108,8 +108,8 @@ window.constants = {
     displayName: "Solar mass (M<sub>⊙</sub>)",
     definition: "Mass of the Sun.",
     values: {
-      "kg": 1.989*10^30,
-      "g": 1.989*10^33
+      "kg": 1.989e30,
+      "g": 1.989e33
     }
   },
 
@@ -118,8 +118,8 @@ window.constants = {
     displayName: "Milky Way mass (M<sub>MW</sub>)",
     definition: "Estimated total mass of the Milky Way galaxy (stars + gas + dark matter).",
     values: {
-      "M<sub>⊙</sub>": 1.5*10^12,
-      "kg": 2.98*10^42
+      "M<sub>⊙</sub>": 1.5e12,
+      "kg": 2.98e42
     }
   },
 
@@ -128,10 +128,10 @@ window.constants = {
     displayName: "Reduced Planck constant (ℏ)",
     definition: "Planck constant divided by 2π, used in quantum mechanics.",
     values: {
-      "J*s": 1.054571817*10^-34,        // h / 2π
-      "erg*s": 1.054571817*10^-27,      // h / 2π
-      "eV*s": 6.582119569*10^-16,       // h / 2π
-      "keV*s": 6.582119569*10^-19       // h / 2π
+      "J*s": 1.054571817e-34,        // h / 2π
+      "erg*s": 1.054571817e-27,      // h / 2π
+      "eV*s": 6.582119569e-16,       // h / 2π
+      "keV*s": 6.582119569e-19       // h / 2π
     }
   },
 
@@ -140,10 +140,10 @@ window.constants = {
   displayName: "Solar Luminosity (L☉)",
   definition: "Total power output of the Sun across all wavelengths.",
   values: {
-    "W": 3.828*10^26,
-    "J/s": 3.828*10^26,
-    "kg*m^2/s^3": 3.828*10^26,
-    "erg/s": 3.828*10^33
+    "W": 3.828e26,
+    "J/s": 3.828e26,
+    "kg*m^2/s^3": 3.828e26,
+    "erg/s": 3.828e33
   }
 },
 
@@ -152,8 +152,8 @@ parsec: {
   displayName: "Parsec (pc)",
   definition: "Distance at which 1 astronomical unit subtends an angle of one arcsecond.",
   values: {
-    "m": 3.0857*10^16,
-    "km": 3.0857*10^13,
+    "m": 3.0857e16,
+    "km": 3.0857e13,
     "ly": 3.26156,
     "AU": 206265
   }
@@ -164,9 +164,9 @@ light_year: {
   displayName: "Light-year (ly)",
   definition: "Distance that light travels in one Julian year in vacuum.",
   values: {
-    "m": 9.4607*10^15,
-    "km": 9.4607*10^12,
-    "mi": 5.8786*10^12,
+    "m": 9.4607e15,
+    "km": 9.4607e12,
+    "mi": 5.8786e12,
     "AU": 63241.1
   }
 },
@@ -178,8 +178,8 @@ joule: {
   values: {
     "N*m": 1,
     "kg*m^2/s^2": 1,
-    "erg": 1*10^7,
-    "eV": 6.241509074*10^18
+    "erg": 1e7,
+    "eV": 6.241509074e18
   }
 },
 
@@ -189,7 +189,7 @@ newton: {
   definition: "SI unit of force; the force required to accelerate one kilogram by one meter per second squared.",
   values: {
     "kg*m/s^2": 1,
-    "dyn": 1*10^5,
+    "dyn": 1e5,
     "lbf": 0.2248089431
   }
 },
@@ -222,8 +222,8 @@ mass_of_proton: {
   displayName: "Proton Mass (mₚ)",
   definition: "Rest mass of a proton.",
   values: {
-    "kg": 1.67262192369*10^-27,
-    "g": 1.67262192369*10^-24,
+    "kg": 1.67262192369e-27,
+    "g": 1.67262192369e-24,
     "u": 1.007276466621,
     "MeV/c^2": 938.27208816
   }
@@ -234,9 +234,9 @@ mass_of_electron: {
   displayName: "Electron Mass (mₑ)",
   definition: "Rest mass of an electron.",
   values: {
-    "kg": 9.1093837015*10^-31,
-    "g": 9.1093837015*10^-28,
-    "u": 5.48579909065*10^-4,
+    "kg": 9.1093837015e-31,
+    "g": 9.1093837015e-28,
+    "u": 5.48579909065e-4,
     "MeV/c^2": 0.510998950
   }
 },
